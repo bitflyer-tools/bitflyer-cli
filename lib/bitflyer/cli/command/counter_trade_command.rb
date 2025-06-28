03949341074f2358bd3dc92f1c552c7a012cdfef
Yuji Ueki <unhappychoice@gmail.com>
2025-06-28 23:08:22 +0900

    :shirt: Obey Rubocop

--- lib/bitflyer/cli/command/counter_trade_command.rb
+++ lib/bitflyer/cli/command/counter_trade_command.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 require 'bitflyer/cli/has_http_client'
 
 class CounterTradeCommand
   include HasHTTPClient
 
-  def run
+  def run # rubocop:disable Metrics/MethodLength
     position = Position.new(http_private_client.positions)
     size = position.size.abs.to_f
-    type = position.size > 0 ? 'SELL' : 'BUY'
+    type = position.size.positive? ? 'SELL' : 'BUY'
     response = http_private_client.send_child_order(
-        product_code: 'FX_BTC_JPY',
-        child_order_type: 'MARKET',
-        side: type,
-        size: size
+      product_code: 'FX_BTC_JPY',
+      child_order_type: 'MARKET',
+      side: type,
+      size: size
     )
     if response['child_order_acceptance_id'].nil?
-      puts 'An error has occurred' + response.to_s
+      puts "An error has occurred#{response}"
     else
       puts "Clear position #{type} / #{size}"
     end
--- lib/bitflyer/cli/command/order_by_best_command.rb
+++ lib/bitflyer/cli/command/order_by_best_command.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require 'bitflyer/cli/has_http_client'
 
 class OrderByBestCommand
   include HasHTTPClient
 
-  def run(options)
+  def run(options) # rubocop:disable Metrics/MethodLength
     amount = options.amount
     type = options.type
 
@@ -11,15 +13,15 @@ class OrderByBestCommand
     price = type == 'buy' ? ticker['best_bid'] : ticker['best_ask']
 
     response = http_private_client.send_child_order(
-        product_code: 'FX_BTC_JPY',
-        child_order_type: 'LIMIT',
-        side: type.upcase,
-        price: price,
-        size: amount
+      product_code: 'FX_BTC_JPY',
+      child_order_type: 'LIMIT',
+      side: type.upcase,
+      price: price,
+      size: amount
     )
 
     if response['child_order_acceptance_id'].nil?
-      puts 'An error has occurred' + response.to_s
+      puts "An error has occurred#{response}"
     else
       puts "An order is created #{type} / #{price} / #{amount}"
     end
